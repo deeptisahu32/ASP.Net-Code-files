bcdd7ac04f86170259d9a7a0d8a9dbae94756aa7
Deepti <Deepti.Sahu@infinite.com>
2026-01-01 13:25:53 +0530

    Adding 31 dec practice files MVC

---
+++ 31 dec/MVC/FirstMVC/Controllers/DemoController.cs
@@ -0,0 +1,64 @@
+﻿using FirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using FirstMVC.Models;
+
+namespace FirstMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //1. normal method
+        public string NormalMethod()
+        {
+            return "Hello , How are you?";
+        }
+
+        //2. viewresult
+
+        public ViewResult ViewMethod()
+        {
+            return View();
+        }
+
+        //3. Contentresult
+        public ContentResult ContentMethod()
+        {
+            // return Content("Hello All!! this is the content","text/plain");
+            return Content("<h1 style=color:blue;> How are you all?</h1>");
+        }
+
+        //4. emptyresult
+        public EmptyResult EmptyMethod()
+        {
+            int amount = 45000;
+            float si = (amount * 3 * 2) / 100;
+            return new EmptyResult();         
+        }
+
+        // 5. redirectresult
+
+        public ActionResult redirectMethod()
+        {
+            //return RedirectToAction("ContentMethod"); //redirecting to other action method of the same controller
+
+            return RedirectToAction("index", "Home");  //for diffrent controller
+        }
+
+        // 6. JsonResult
+
+        public JsonResult JsonMethod()
+        {
+            Employee employee=new Employee() { Id=1,Name="Deepa",Age=23};  //object intializer without calling cunstucture
+            return Json(employee,JsonRequestBehavior.AllowGet); 
+        }
+    }
+}
---
+++ 31 dec/MVC/FirstMVC/Controllers/HRController.cs
@@ -0,0 +1,55 @@
+﻿using FirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class HRController : Controller
+    {
+        // GET: HR
+
+        //3. calling another view and passing the model object 
+
+        public ActionResult Index()
+        {
+            List<Department>deptlist=new List<Department>()
+            {
+                new Department{Id=100,DeptName="CSE"},
+                new Department{Id=200,DeptName="ECE"},
+                new Department{Id=300,DeptName="IT"},
+                new Department{Id=400,DeptName="EEE"}
+            };
+            return View("DepartmentList",deptlist);
+        }
+
+        //the recieving view
+        public ActionResult DepartmentList(List<Department> d)
+        {
+            return View(d);
+        }
+
+        //1. inding a model object to a view
+
+        public ActionResult DisplayEmployee()
+        {
+            Employee employee = new Employee() { Id=2,Name="Ankita",Age=22};
+            return View(employee); //passing a model object of type employee
+        }
+
+        //2. binding a collection model object to a view
+        public ActionResult EmployeeList()
+        {
+            List<Employee> emplist = new List<Employee>()
+            {
+                new Employee{Id=3,Name="Ram",Age=32},
+                new Employee{Id=4,Name="Radhe",Age=25},
+                new Employee{Id=5,Name="Shobha",Age=30}
+            };
+            return View(emplist);
+        }
+
+    }
+}
